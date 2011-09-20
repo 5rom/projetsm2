ce6f8411b69feb96025f3b964048b6c0eac6460f
crescence.david@gmail.com <crescence.david@gmail.com@8af57af2-50e6-298e-af2d-741582e57622>
2011-09-20 19:39:05 +0000

    First commit

---
+++ trunk/TI1/TP1/trunk/src/annuaire/SiteDAO.java
@@ -0,0 +1,10 @@
+package annuaire;
+
+import java.util.ArrayList;
+
+public interface SiteDAO {
+    public void addSite(Site item);
+    public void deleteSite(Site item);
+    public ArrayList<Site> getAllSites();
+    public String getInfos();
+}
