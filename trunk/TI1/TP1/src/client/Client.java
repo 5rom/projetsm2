a22f65666ce8eec88e973e952532d00389833831
sebastien.faure3@gmail.com <sebastien.faure3@gmail.com@8af57af2-50e6-298e-af2d-741582e57622>
2011-09-22 20:02:35 +0000

    Question 2.1 finie!

--- trunk/TI1/TP1/src/client/Client.java
+++ trunk/TI1/TP1/src/client/Client.java
@@ -1,14 +1,55 @@
 package client;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
+import serveur.Serveur;
+
 import annuaire.Annuaire;
+import annuaire.Site;
 
 public class Client {
 
-	Annuaire annu;
+	// La reference du serveur (voir si necessaire de le garder)
+	Serveur serv;
 	
-	public Client(Annuaire annuaire) {
-		// TODO Auto-generated constructor stub
-		this.annu=annuaire;
+	public Client(){
+		
 	}
+	
+	/**
+	 * Enlevé car le client ne doit pas avoir
+	 * accès à la référence de l'annuaire du serveur,
+	 * dangereux!
+	 * @param annuaire
+	 */
+	//Annuaire annu;
+	// Recupération de l'annuaire
+	//public void setAnnuaire(Annuaire annuaire){
+	//	this.annu=annuaire;
+	//}
+	
+	
+	/**
+	 * Desormais le client utilise l'API du serveur
+	 * pour manipuler l'annuaire.
+	 */
+    private void addSite(String desc, String url) {
+		HashMap<String, String> hm = new HashMap<String, String>();
+		hm.put("desc", desc);
+		hm.put("url", url);	
+        serv.traiteRequete("addSite", hm);
+    }
+
+    private void removeSite(String desc, String url) {
+		HashMap<String, String> hm = new HashMap<String, String>();
+		hm.put("desc", desc);
+		hm.put("url", url);	
+        serv.traiteRequete("removeSite", hm);  	
+    }
+
+    private String listSites() {
+        return serv.traiteRequete("listSites", null);
+    }
 
 }
--- trunk/TI1/TP1/src/main.java
+++ trunk/TI1/TP1/src/main.java
@@ -10,7 +10,11 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Serveur serv = new Serveur();
-		Client cli = new Client(serv.getAnnuaire());
+		Client cli = new Client();
+		
+		// Retiré volontairement à la partie 2 car dangereux
+		// d'envoyer la reference de l'annuaire du serveur à un client!
+		//cli.setAnnuaire(serv.getAnnuaire());
 	}
 
 }
