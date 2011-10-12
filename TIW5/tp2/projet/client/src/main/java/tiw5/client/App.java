af0b591c152a4ebda0c214a7de0d9b84d57f29c2
sebastien.faure3 <sebastien.faure3@gmail.com>
2011-10-12 21:07:51 +0000

    TIW5 TP2 : Utilisation des classes générées pour le client par le plugin cxf pour maven.

--- TIW5/tp2/projet/client/src/main/java/tiw5/client/App.java
+++ TIW5/tp2/projet/client/src/main/java/tiw5/client/App.java
@@ -1,5 +1,7 @@
 package tiw5.client;
 
+import fr.univ_lyon1.master_info.m2ti.tiw5.services.Album;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        /**
+         * Ici faire:
+         * Utiliser les classes Java générées pour réaliser un client en ligne de
+         * commande pour le service précédent. Si l'argument en ligne de commande
+         * est un numéro, alors le client récupérera la description XML de
+         * l'album correspondant, sinon, et argument sera un nom de fichier
+         * xml contenant un descriptif d'album à insérer dans les données.
+         */
+        
+        Album a;
     }
 }
