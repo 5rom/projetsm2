37d7a825e2a3445352d97952aa55def0b4c6e98d
sebastien.faure3@gmail.com <sebastien.faure3@gmail.com@8af57af2-50e6-298e-af2d-741582e57622>
2011-10-14 08:24:21 +0000

    TI1 TP3 : nettoyage

--- trunk/TI1/TP3/serveur/runServeur.bat
+++
@@ -1,3 +0,0 @@
-javac serveur/*.java
-rmic serveur.HelloImpl
-java -Djava.rmi.server.codebase=file:C:/Users/toto/RMI/src/serveur/ -Djava.rmi.server.hostname=192.168.1.38 -Djava.security.policy=server.policy serveur.Serveur
--- trunk/TI1/TP3/serveur/serveur/Hello.java
+++
@@ -1,8 +0,0 @@
-package serveur;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Hello extends Remote {
-	public String sayHello(String nom) throws RemoteException;
-}
