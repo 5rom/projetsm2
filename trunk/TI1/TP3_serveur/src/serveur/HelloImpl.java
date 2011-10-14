382b41ef77c61668392720cfc22f839ffdf147da
sebastien.faure3@gmail.com <sebastien.faure3@gmail.com@8af57af2-50e6-298e-af2d-741582e57622>
2011-10-14 09:29:20 +0000

    TI1 TP3 : Lancement du serveur.

---
+++ trunk/TI1/TP3_serveur/src/serveur/HelloImpl.java
@@ -0,0 +1,16 @@
+package serveur;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class HelloImpl extends UnicastRemoteObject implements Hello {
+	private static final long serialVersionUID = 1L;
+
+	protected HelloImpl() throws RemoteException {
+		super();
+	}
+
+	public String sayHello(String nom) throws RemoteException{
+		return "Hello " + nom + "!!!";
+	}
+}
