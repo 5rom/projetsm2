883b2732ac2ee00403c45f1766804dac3799a0d3
crescence.david <crescence.david@gmail.com>
2011-10-14 08:59:45 +0000

    TI1 TP3 - Restructuration de l'arborescence.

--- TI1/TP3/client/runClient.bat
+++
@@ -1,4 +0,0 @@
-javac client/*.java
-rmic client.HelloImpl
-java -Djava.rmi.server.codebase=file:/C:/Users/toto/RMI/src/client/ -Djava.security.policy=client.policy client.Client 192.168.1.38
-@pause
