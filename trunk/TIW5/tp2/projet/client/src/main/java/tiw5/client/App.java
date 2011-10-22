037bd5788d35eaf861067f87784fc925f4519ab1
sebastien.faure3@gmail.com <sebastien.faure3@gmail.com@8af57af2-50e6-298e-af2d-741582e57622>
2011-10-22 10:28:49 +0000

    TIW5 TP2 : Commentaires dans les projets modele, client (ok) et services-impl (commencé : voir pourquoi le wsdl met un warning de validation que l'on avait pas avant).

--- trunk/TIW5/tp2/projet/client/src/main/java/tiw5/client/App.java
+++ trunk/TIW5/tp2/projet/client/src/main/java/tiw5/client/App.java
@@ -2,38 +2,45 @@ package tiw5.client;
 
 import java.io.File;
 import java.io.StringWriter;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import fr.univ_lyon1.master_info.m2ti.tiw5.services.Album;
 import fr.univ_lyon1.master_info.m2ti.tiw5.services.AlbumDataPortType;
 import fr.univ_lyon1.master_info.m2ti.tiw5.services.AlbumDataService;
 
+/**
+ * Classe App.java
+ * Implémentation d'un client en ligne de commandes pour utiliser le 
+ * service AlbumDataService en demandant l'ajout d'albums et leur description
+ * @author David CRESCENCE <crescence.david@gmail.com> et Sébastien FAURE <sebastien.faure3@gmail.com>
+ * UCBL M2TI 2011-2012 
+ */
 public class App 
 {
-	
+	/**
+	 * Logger pour afficher les informations de debug
+	 */
 	private static final Logger log = LoggerFactory.getLogger(App.class);
 	
+	/**
+     * Utilisation des classes Java générées par cxf pour réaliser
+     * un client en ligne de commande pour le service AlbumDataService.
+     * Si l'argument en ligne de commande est un numéro,
+     * alors le client récupérera la description XML de
+     * l'album correspondant, sinon, et argument sera un nom de fichier
+     * xml contenant un descriptif d'album à insérer dans les données.
+	 * @param args
+	 */
     public static void main( String[] args )
     {
-
-        /**
-         * Ici faire:
-         * Utiliser les classes Java générées pour réaliser un client en ligne de
-         * commande pour le service précédent. Si l'argument en ligne de commande
-         * est un numéro, alors le client récupérera la description XML de
-         * l'album correspondant, sinon, et argument sera un nom de fichier
-         * xml contenant un descriptif d'album à insérer dans les données.
-         */
-        
+    	/**
+    	 * On vérifie que le nombre d'arguments est le bon
+    	 */
     	if (args.length==1){
 		        try {
-		        	
 		        	/*
 		        	 * Si c'est un nombre : alors on veut recuperer un album
 		        	 */
@@ -78,25 +85,31 @@ public class App
 			            log.info("Création du contexte JAXB...");
 				        JAXBContext context = JAXBContext.newInstance(AlbumDescription.class);
 				   
-				        // Instanciation du marshaller (objet faisant la generation du XML en fonction de nos annotations dans les classes metiers)
+				        // Instanciation d' l'unmarshaller (objet désérialiseur construisant l'objet a partir de sa description en XML)
 				        log.info("Création du unmarshaller JAXB...");
 				        Unmarshaller un = context.createUnmarshaller();
 		
 				        log.info("Désérialisation...");
+				        // Désérialisation
 			        	AlbumDescription aD = (AlbumDescription)un.unmarshal(new File(args[0]));
 		
 			        	log.info("Ajout de l'album créé dans la base...");
+			        	// Demande au service AlbumService d'execution de l'opération d'ajout d'album
 			        	aDPT.addAlbumDescription(aD.getAlbum());
+			        	log.info("Album enregistré avec succès.");
 		        	
 		        	} catch (Exception ex){
+		        		// Le fichier n'existe pas ou est incorrect
 		        		log.error("Erreur d'ajout d'album depuis un fichier xml!");
 		        		ex.printStackTrace();
 		        	}
 	        	} else {
+	        		// L'argument n'est ni un nombre ni un nom de fichier
 	        		log.error("Usage: java App [idAlbum|xmlAlbumDescriptionFilePath]");
 	        	}
 	        }
     	} else {
+    		// On rappelle coté client comment utiliser le programme
     		log.error("Usage: java App [idAlbum|xmlAlbumDescriptionFilePath]");
     	}
     }
--- trunk/TIW5/tp2/projet/modele/src/main/java/tiw5/modele/Stock.java
+++ trunk/TIW5/tp2/projet/modele/src/main/java/tiw5/modele/Stock.java
@@ -3,27 +3,51 @@ package tiw5.modele;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-
-
+/**
+ * Classe Stock
+ * Classe ajoutée au modèle pour représenter un stock d'albums et montrer
+ * que l'ajout du nouveau service de gestion d'un entrepôt d'albums fonctionne
+ * Dans un souci de simplicité, on n'ajoute pas de dépendance vers
+ * la classe Album ce qui fait que les tables Album et Stock ne sont pas liées
+ * par une relation. Donc l'id d'album de l'entité stock bien que représentant
+ * un identifiant d'album tel que le champ id de la table album n'est pas une
+ * clé étrangère au sens propre (pas de contrainte).
+ * @author David CRESCENCE <crescence.david@gmail.com> et Sébastien FAURE <sebastien.faure3@gmail.com>
+ * UCBL M2TI 2011-2012 
+ */
 @Entity
 public class Stock {
 	
+	/**
+	 * Identifiant de l'album dont on veut connaitre la quantité
+	 */
 	@Id
 	private long albumID;
 	
+	/**
+	 * Quantité de l'album ayant pour id album id
+	 */
 	private long quantite;
 	
+	/**
+	 * Constructeur à utiliser
+	 * @param album L'id de l'album a stocker
+	 * @param qte La quantité de cd de cet album a stocker
+	 */
 	public Stock(long album, long qte) {
 		this.albumID = album;
 		this.quantite = qte;
 	}
 	
+	/**
+	 * Constructeur par défaut : vide
+	 */
 	public Stock() {
 	}	
 	
 	/**
 	 * Modifieur de l'identifiant de l'album
-	 * @param id
+	 * @param id Le nouvel id
 	 */
 	public void setAlbumId(long id){
 		this.albumID=id;
@@ -31,14 +55,15 @@ public class Stock {
 
 	/**
 	 * Accesseur de l'identifiant de l'album
+	 * @return L'id d'album stocké
 	 */
 	public long getAlbumId(){
 		return albumID;
 	}	
 	
 	/**
-	 * Modifieur de la quantité de l'album
-	 * @param qte
+	 * Modifieur de la quantité de cd en stock pour cet album
+	 * @param qte La nouveau nombre de cd en stock pour cet album
 	 */
 	public void setQuantite(long qte){
 		this.quantite=qte;
@@ -46,6 +71,7 @@ public class Stock {
 	
 	/**
 	 * Accesseur de la quantité de l'album
+	 * @return La nombre de cd en stock pour cet album
 	 */
 	public long getQuantite(){
 		return quantite;
