aabdb332c38f621253772f9bea764cf9b61c9618
sebastien.faure3@gmail.com <sebastien.faure3@gmail.com@8af57af2-50e6-298e-af2d-741582e57622>
2011-11-18 09:50:11 +0000

    TI1 TP5 : nouveau projet apres passage sous eclipse

---
+++ trunk/TI1/TP5/ti1tp5new/annuaire-bundles/annuaire/src/main/java/tp5/serveur/Serveur.java
@@ -0,0 +1,8 @@
+package tp5.serveur;
+
+import java.util.HashMap;
+
+public interface Serveur {
+	public String traiteRequete(String commande, HashMap<String, String> parametres);
+	public Annuaire getAnnuaire();
+}
---
+++ trunk/TI1/TP5/ti1tp5new/annuaire-bundles/annuaire/src/tp1/client/AnnuaireUI.java
@@ -0,0 +1,114 @@
+package tp1.client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import tp1.Site;
+import tp1.SiteXMLDAO;
+
+import tp1.serveur.Annuaire;
+
+public class AnnuaireUI {
+
+	//Variable globale (singleton)
+	static Annuaire annuaire;
+	
+	public static void main (String argv[]) {
+		//annuaire = new Annuaire("test.xml");
+		SiteXMLDAO xdao = new SiteXMLDAO("test.xml");
+		ArrayList<Site> sites = new ArrayList<Site>();
+		annuaire = new Annuaire(sites,xdao);
+        annuaire.initSites();
+        
+		String description = null;
+		String url = null;
+		
+		for (;;) {
+			menu();
+			int c = new String (readLine()).charAt(0) - '0';
+			switch (c){
+			case 1:
+				try {
+					System.out.print("Description : ");
+					description = readLine();
+					//System.in.read(d);
+					System.out.print("URL : ");
+					url = readLine();
+					//System.in.read(u);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				//annuaire.addSite(description, url);
+				HashMap<String, String> hm = new HashMap<String, String>();
+				hm.put("desc", description);
+				hm.put("url", url);
+				System.out.println("Ajout du site :"+description+" "+url);
+				annuaire.process("addSite", hm);	
+				break;
+
+			case 2:
+				try {
+					System.out.print("Description : ");
+					description = readLine();
+					//System.in.read(d);
+					System.out.print("URL : ");
+					url = readLine();
+					//System.in.read(u);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				//annuaire.removeSite(description, url);
+				hm = new HashMap<String, String>();
+				hm.put("desc", description);
+				hm.put("url", url);
+				annuaire.process("removeSite", hm);
+				break;
+				
+			case 3:
+				System.out.println("Sites actuellement connus :\n\n");
+				//System.out.println(annuaire.listSites());
+				System.out.println(annuaire.process("listSites", null));
+				break;
+
+			case 4:
+				annuaire.initSites();
+				System.out.println("Liste des sites reinitialisee par rapport au DAO.\n\n");
+				break;
+
+			case 5:
+				return;
+			}
+
+		}
+		
+	}
+	
+	public static void menu() {
+		System.out.println("Menu\n\n");
+		System.out.println("1)\tRajouter un site\n");
+		System.out.println("2)\tSupprimer un site un site\n");
+		System.out.println("3)\tLister les sites\n");
+        System.out.println("4)\tReinitialiser les sites\n");
+        System.out.println("5)\tQuitter\n");
+	}
+
+//	 ---------------------------------------------
+//   Code trouv� � l'URL : http://www.wellho.net/resources/ex.php4?item=j703/WellHouseInput.java
+	   public static String readLine()
+	      {
+	      BufferedReader standard = new BufferedReader(
+	           new InputStreamReader(System.in));
+	      try{
+	         String inline = standard.readLine();
+	         return inline;
+	         }
+	      catch (Exception e)
+	         {
+	         return ("data entry error");
+	         }
+	      }
+
+}
