7e9ec35bde862f9bb02c871eb7a3a4176bf0fe85
sebastien.faure3 <sebastien.faure3@gmail.com>
2011-11-18 09:50:11 +0000

    TI1 TP5 : nouveau projet apres passage sous eclipse

---
+++ TI1/TP5/ti1tp5new/annuaire-bundles/annuaire/src/main/java/tp5/SiteDAO.java
@@ -0,0 +1,10 @@
+package tp5;
+
+import java.util.ArrayList;
+
+public interface SiteDAO {
+    public void addSite(Site item);
+    public void deleteSite(Site item);
+    public ArrayList<Site> getAllSites(ArrayList<Site> liste);
+    public String getInfos();
+}
---
+++ TI1/TP5/ti1tp5new/annuaire-bundles/annuaire/src/main/java/tp5/serveur/Activator.java
@@ -0,0 +1,23 @@
+package tp5.serveur;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+public class Activator implements BundleActivator {
+    
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+        context.registerService(Serveur.class.getName(), new ServeurImpl(), null);
+	}
+    
+	
+	
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+        
+	}
+    
+}
