c04804c5f29643222521c67bdc7a98f4aa27b1bd
sebastien.faure3@gmail.com <sebastien.faure3@gmail.com@8af57af2-50e6-298e-af2d-741582e57622>
2011-11-27 13:44:01 +0000

    TIW5 TP3 : Fini.

--- trunk/TIW5/tp3/projet/services-impl/src/main/java/tiw5/services/handlers/AccessControlHandler.java
+++ trunk/TIW5/tp3/projet/services-impl/src/main/java/tiw5/services/handlers/AccessControlHandler.java
@@ -1,5 +1,7 @@
 package tiw5.services.handlers;
 
+import java.util.logging.Logger;
+
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
@@ -15,12 +17,32 @@ import org.w3c.dom.Document;
 import org.w3c.dom.DocumentFragment;
 import org.w3c.dom.Node;
 
-import tiw5.user.UtilisateurID;
-import tiw5.util.services.util.Utils;
+import tiw5.modele.user.UtilisateurID;
+import tiw5.services.util.Utils;
 
+/**
+ * Classe AccessControlHandler
+ * Intercepteur logique de controle d'acces pour les utilisateurs.
+ * Vérifie que l'utilisateur (renseigné dans le contexte) a bien le droit de modifier un album.
+ * La liste des utilisateurs avec leurs droits est stockée dans un fichier XML dont le chemin est parametrable ici.
+ * @author David CRESCENCE <crescence.david@gmail.com> et Sébastien FAURE <sebastien.faure3@gmail.com>
+ * UCBL M2TI 2011-2012 
+ */
 public class AccessControlHandler implements LogicalHandler<LogicalMessageContext> {
+
+	/**
+	 * Le logger
+	 */
+	private static Logger log = Logger.getLogger("AccessControlHandler");
+	
+	/**
+	 * Transformer pour le message en XML
+	 */
 	private Transformer copy;
 
+	/**
+	 * Constructeur
+	 */
 	public AccessControlHandler() {
 		try {
 			copy = TransformerFactory.newInstance().newTransformer();
@@ -29,6 +51,12 @@ public class AccessControlHandler implements LogicalHandler<LogicalMessageContex
 		}
 	}
 	
+	/**
+	 * Methode getMainMessageElement:
+	 * Permet de recuperer l'element principal du message en XML
+	 * @param arg0 Le contexte du message
+	 * @return String Le nom de l'element principal du message
+	 */
 	public String getMainMessageElement(LogicalMessageContext arg0) {
 			Source payload = arg0.getMessage().getPayload();
 			DOMResult dom = new DOMResult();
@@ -44,39 +72,55 @@ public class AccessControlHandler implements LogicalHandler<LogicalMessageContex
 				root = ((Document)root).getDocumentElement();
 			}
 			return root.getNodeName();
-		}	
+		}
+
+	/**
+	 * Methode close
+	 * Appelee a la fin d'un echange de message
+	 */
 	@Override
 	public void close(MessageContext arg0) {
-		// TODO Auto-generated method stub
-		
 	}
 
+	/**
+	 * Methode handleFault
+	 * Appelee en cas d'erreur de traitement d'un message
+	 */
 	@Override
 	public boolean handleFault(LogicalMessageContext arg0) {
-		// TODO Auto-generated method stub
+		log.warning("AccessControlHandler: Erreur rencontrée: "+getMainMessageElement(arg0));				
+		// Le traitement continue
 		return true;
 	}
 
 
-
+	/**
+	 * Methode handleMessage
+	 * Traitement d'un message : recuperation de l'utilisateur 
+	 * du contexte et controle de son droit de modification d'albums
+	 */
 	@Override
 	public boolean handleMessage(LogicalMessageContext arg0) {
 		boolean sortant = (Boolean) arg0.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
-		if (!sortant){
-			if (getMainMessageElement(arg0).contains("addAlbumDescription")){
-				String userValue=(String) arg0.get("user");
-				String passwordValue=(String) arg0.get("password");
-				UtilisateurID currentUserID = new UtilisateurID(userValue, passwordValue);
-				//Ici on regarde dans le xml s'il y est
-				//S'il n'y est pas on genere l'exception ProtocolException 
-				if (Utils.checkCanEditAlbumFromXMLFile("C:\\Users\\Seb\\eclipse-projects\\svn\\TIW5\\tp3\\projet\\listeIdentifiants.xml", currentUserID)){
-					System.out.println("AccessControlHandler:L'utilisateur a le droit d'ajouter des albums!");
-				} else {
-					System.out.println("AccessControlHandler:L'utilisateur n'a pas le droit d'ajouter des albums! Erreur!");
-					throw new ProtocolException();
-				}
+		
+		// Si il y a bien un utilisateur dans le contexte
+		if ((!sortant) && (getMainMessageElement(arg0).contains("addAlbumDescription")))
+		{	
+			// Récupération de l'utilisateur depuis le contexte
+			String userValue=(String) arg0.get("user");
+			String passwordValue=(String) arg0.get("password");
+			UtilisateurID currentUserID = new UtilisateurID(userValue, passwordValue);
+		 
+			// Contrôle d'accès pour cet utilisateur
+			if (Utils.checkCanEditAlbumFromXMLFile("C:\\Users\\Seb\\eclipse-projects\\svn\\TIW5\\tp3\\projet\\listeIdentifiants.xml", currentUserID)){
+				log.info("AccessControlHandler:L'utilisateur a le droit d'ajouter/modifier des albums!");
+			} else {
+				log.warning("AccessControlHandler:L'utilisateur n'a pas le droit d'ajouter/modifier des albums! Erreur!");
+				throw new ProtocolException();
 			}
 		}
+		
+		// On continue de traiter les messages
 		return true;
 	}
 
--- trunk/TIW5/tp3/projet/services-impl/src/main/java/tiw5/services/handlers/UserHandler.java
+++ trunk/TIW5/tp3/projet/services-impl/src/main/java/tiw5/services/handlers/UserHandler.java
@@ -1,6 +1,7 @@
 package tiw5.services.handlers;
 
 import java.util.Set;
+import java.util.logging.Logger;
 
 import javax.xml.namespace.QName;
 import javax.xml.soap.SOAPEnvelope;
@@ -11,40 +12,65 @@ import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
 
+/**
+ * Classe UserHandler
+ * Intercepteur SOAP d'enrichissement du contexte par les 
+ * identifiants d'un utilisateur passés dans le header de la requète reçue.
+ * @author David CRESCENCE <crescence.david@gmail.com> et Sébastien FAURE <sebastien.faure3@gmail.com>
+ * UCBL M2TI 2011-2012 
+ */
 public class UserHandler implements SOAPHandler<SOAPMessageContext>{
 
+	/**
+	 * Logger
+	 */
+	private static Logger log = Logger.getLogger("LogHandler");
+	
+	
+	/**
+	 * Methode close
+	 * Appelee a la fin d'un echange de message
+	 */
 	@Override
 	public void close(MessageContext context) {
-		// TODO Auto-generated method stub
-
 	}
 
+	/**
+	 * Methode handleFault
+	 * Appelee en cas d'erreur de traitement d'un message
+	 */
 	@Override
 	public boolean handleFault(SOAPMessageContext context) {
-		// TODO Auto-generated method stub
-		return false;
+		log.warning("UserHandler: Erreur rencontrée...");				
+		// Le traitement continue		
+		return true;
 	}
 
+	/**
+	 * Methode handleMessage
+	 * Traitement d'un message : recuperation de l'utilisateur 
+	 * du contexte et controle de son droit de modification d'albums
+	 */	
 	@Override
 	public boolean handleMessage(SOAPMessageContext context) {
-		Boolean isRequest = (Boolean) context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+		Boolean sortant = (Boolean) context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
 
-		//for response message only, true for outbound messages, false for inbound
-		if(!isRequest){
+		// Seulement pour les messages entrants
+		if(!sortant){
 
 			try{
+				// Récupération du header
 				SOAPMessage soapMsg = context.getMessage();
 				SOAPEnvelope soapEnv = soapMsg.getSOAPPart().getEnvelope();
 				SOAPHeader soapHeader = soapEnv.getHeader();
 
-				//if no header, add one
+				// S'il n'y a pas d'header on en ajoute un
 				if (soapHeader == null){
 					soapHeader = soapEnv.addHeader();
 				} else {
 					// s'il y en a un on cherche s'il y'a user et password
 					if (soapHeader.getElementsByTagNameNS("http://master-info.univ-lyon1.fr/M2TI/TIW5/services", "user").getLength()>0){
 						// Element user present
-						//System.out.println("user present:"+soapHeader.getElementsByTagNameNS("http://master-info.univ-lyon1.fr/M2TI/TIW5/services", "user").item(0).getTextContent());
 						context.put("user", (soapHeader.getElementsByTagNameNS("http://master-info.univ-lyon1.fr/M2TI/TIW5/services", "user").item(0).getTextContent()));
 					}
 					if (soapHeader.getElementsByTagNameNS("http://master-info.univ-lyon1.fr/M2TI/TIW5/services", "password").getLength()>0){
@@ -52,17 +78,19 @@ public class UserHandler implements SOAPHandler<SOAPMessageContext>{
 						context.put("password", (soapHeader.getElementsByTagNameNS("http://master-info.univ-lyon1.fr/M2TI/TIW5/services", "password").item(0).getTextContent()));
 					}		    	 
 				}
-
 			}catch(SOAPException e){
 				System.err.println(e);
 			}
-
 		}
 
-		//continue other handler chain
+		// On continue le traitement des messages
 		return true;
 	}
 
+	/**
+	 * Methode getHeaders():
+	 * Renvoie les headers
+	 */
 	@Override
 	public Set<QName> getHeaders() {
 		// TODO Auto-generated method stub
