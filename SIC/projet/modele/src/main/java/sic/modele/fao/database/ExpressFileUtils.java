abd1efbdf29f53d4eca8360ecb479df5eacb7269
crescence.david <crescence.david@gmail.com>
2012-01-06 10:23:33 +0000

    Commit.

---
+++ SIC/projet/modele/src/main/java/sic/modele/fao/database/ExpressFileUtils.java
@@ -0,0 +1,9 @@
+package sic.modele.fao.database;
+
+import gov.nist.msid.ontostep.schema.ExpressParser;	
+
+public class ExpressFileUtils {
+
+	//EasyParser parser = new ExpressParser("file.exp");
+	
+}
