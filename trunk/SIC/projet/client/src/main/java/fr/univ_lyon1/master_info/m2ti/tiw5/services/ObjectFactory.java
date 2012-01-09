d630ed1186611e894722c793f7e1b2520805cfb7
sebastien.faure3@gmail.com <sebastien.faure3@gmail.com@8af57af2-50e6-298e-af2d-741582e57622>
2012-01-09 22:26:41 +0000

    SIC WS BD consultation du contenu de la bd

--- trunk/SIC/projet/client/src/main/java/fr/univ_lyon1/master_info/m2ti/tiw5/services/ObjectFactory.java
+++ trunk/SIC/projet/client/src/main/java/fr/univ_lyon1/master_info/m2ti/tiw5/services/ObjectFactory.java
@@ -25,6 +25,7 @@ import javax.xml.namespace.QName;
 public class ObjectFactory {
 
     private final static QName _AddProduitResponse_QNAME = new QName("http://master-info.univ-lyon1.fr/M2TI/TIW5/services", "addProduitResponse");
+    private final static QName _GetProduitListResponse_QNAME = new QName("http://master-info.univ-lyon1.fr/M2TI/TIW5/services", "getProduitListResponse");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: fr.univ_lyon1.master_info.m2ti.tiw5.services
@@ -42,11 +43,35 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link GetProduitList }
+     * 
+     */
+    public GetProduitList createGetProduitList() {
+        return new GetProduitList();
+    }
+
+    /**
+     * Create an instance of {@link PnumPnom }
+     * 
+     */
+    public PnumPnom createPnumPnom() {
+        return new PnumPnom();
+    }
+
+    /**
      * Create an instance of {@link AddProduitResponse }
      * 
      */
     public AddProduitResponse createAddProduitResponse() {
         return new AddProduitResponse();
+    }
+
+    /**
+     * Create an instance of {@link GetProduitListResponse }
+     * 
+     */
+    public GetProduitListResponse createGetProduitListResponse() {
+        return new GetProduitListResponse();
     }
 
     /**
@@ -56,6 +81,15 @@ public class ObjectFactory {
     @XmlElementDecl(namespace = "http://master-info.univ-lyon1.fr/M2TI/TIW5/services", name = "addProduitResponse")
     public JAXBElement<AddProduitResponse> createAddProduitResponse(AddProduitResponse value) {
         return new JAXBElement<AddProduitResponse>(_AddProduitResponse_QNAME, AddProduitResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link GetProduitListResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://master-info.univ-lyon1.fr/M2TI/TIW5/services", name = "getProduitListResponse")
+    public JAXBElement<GetProduitListResponse> createGetProduitListResponse(GetProduitListResponse value) {
+        return new JAXBElement<GetProduitListResponse>(_GetProduitListResponse_QNAME, GetProduitListResponse.class, null, value);
     }
 
 }
